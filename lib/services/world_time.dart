99c4b00f367cb42fd4543c82453bf19e259297cf
Tars <taa522@Mail.umkc.edu>
2020-01-21 07:06:12 -0600

    ternary operator

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -6,6 +6,7 @@ class WorldTime{
   String time; //time in that location
   String flag; //url to an asset flag icon
   String url; //location url for api endpoint
+  bool isDayTime;
 
 
   WorldTime({this.location, this.flag, this.url});
@@ -30,6 +31,7 @@ class WorldTime{
       now = now.add(Duration(hours: int.parse(offset)));
 
       //set the time property
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
     } catch(e){
       print('Caught error: $e');
